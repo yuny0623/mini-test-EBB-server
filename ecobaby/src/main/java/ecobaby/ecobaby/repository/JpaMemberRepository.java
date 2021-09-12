b3481c32433fc35ace3899e89ffdedfcac67e93b
yuny0623 <overman522@gmail.com>
2021-09-12 14:46:55 +0900

    @Transactional is added

--- ecobaby/src/main/java/ecobaby/ecobaby/repository/JpaMemberRepository.java
+++ ecobaby/src/main/java/ecobaby/ecobaby/repository/JpaMemberRepository.java
@@ -3,12 +3,14 @@ package ecobaby.ecobaby.repository;
 import ecobaby.ecobaby.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
+@Transactional
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager em;
